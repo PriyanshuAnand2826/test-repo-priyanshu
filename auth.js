1e813b3b4874e87d4727af4a3c97a2a8cc7df9b4
Priyanshu <priyanshuanand60@gmail.com>
2024-03-27 23:04:19 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken")
 const model = require("./model")
 
+
 const auth = async (req, resp, next) => {
     try {
         const token = req.body.jwt;
         console.log(req.body);
-        const verify = jwt.verify(token, process.env.secretKey)
+        const verify = jwt.verify(token, process.env.secretkey)
         const user = await model.findOne({ _id: verify._id })
 
         console.log("user details", user);
